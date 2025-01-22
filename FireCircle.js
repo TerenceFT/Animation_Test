c313ef92be2756f1f0bac39cbe494db15ecaa439
Terence F Trajano <ttrajano@uw.edu>
2025-01-22 10:16:04 -0800

    first commit

---
+++ FireCircle.js
@@ -0,0 +1,13 @@
+class FireCircle {
+    constructor() {
+
+    }
+
+    update() {
+
+    }
+
+    draw(ctx) {
+        ctx.drawImage(ASSET_MANAGER.getAsset("./fire_circles_100x100_61f"),0,0);
+    }
+}
